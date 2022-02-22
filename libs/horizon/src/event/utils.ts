1ed7e608cc5e3246c874e9df4484b969914a1ab5
* <8>
2022-02-22 10:13:35 +0800

    Match-id-6208d162f4af32a98c0b3a08ba5648aefc074f3b

--- libs/horizon/src/event/utils.ts
+++ libs/horizon/src/event/utils.ts
@@ -6,15 +6,6 @@ export function isInputElement(dom?: HTMLElement): boolean {
   return false;
 }
 
-
-// 例：dragEnd -> onDragEnd
-export function addOnPrefix(name) {
-  if (!name) {
-    return '';
-  }
-  return 'on' + name[0].toUpperCase() + name.slice(1);
-}
-
 export function setPropertyWritable(obj, propName) {
   const desc = Object.getOwnPropertyDescriptor(obj, propName);
   if (!desc || !desc.writable) {
