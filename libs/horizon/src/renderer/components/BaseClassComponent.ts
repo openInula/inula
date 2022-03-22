ea44e30933164f272d7411e6d6b637d220a9083c
* <8>
2022-03-22 10:26:15 +0800

    Match-id-5935bd5d4b80a4524d2b9a839ca106c4249ad3e5

--- libs/horizon/src/renderer/components/BaseClassComponent.ts
+++ libs/horizon/src/renderer/components/BaseClassComponent.ts
@@ -2,7 +2,7 @@
  * Component的api setState和forceUpdate在实例生成阶段实现
  */
 
-class Component<P,S,C> {
+class Component<P, S, C> {
   props: P;
   context: C;
   state: S | null;
@@ -15,12 +15,15 @@ class Component<P,S,C> {
   }
 
   setState(state: S) {
-    console.error('Cant not call `this.setState` in the constructor of class component, it will do nothing')
+    if (isDev) {
+      console.error('Cant not call `this.setState` in the constructor of class component, it will do nothing');
+    }
   }
 }
 
 // 兼容三方件 react-lifecycles-compat，它会读取 isReactComponent 属性值，不添加会导致 eview-ui 官网白屏
 Component.prototype.isReactComponent = true;
+
 /**
  * 支持PureComponent
  */
