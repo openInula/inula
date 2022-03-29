3aeaf4969309dc9a99dfff8f3ce017d5bec90539
* <8>
2022-03-29 17:59:47 +0800

    Match-id-7d9916e11a4b9ee889fdb62be87fe24b4d0e5f1c

---
+++ libs/extension/src/svgs/Copy.tsx
@@ -0,0 +1,8 @@
+
+export default function Copy() {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='1rem' height='1rem'>
+      <path d='M0 0 H16 V16 H0 z M2 2 H8 V8 H2 V2z' fill='currentColor' fill-rule='evenodd'/>
+    </svg>
+  )
+}
---
+++ libs/extension/src/svgs/Eye.tsx
@@ -0,0 +1,10 @@
+
+export default function Eye() {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='1rem' height='1rem'>
+      <ellipse cx="8" cy="8" rx="8" ry="6" />
+      <circle cx="8" cy="8" r= "4" fill="rgb(255, 255, 255)"/>
+      <circle cx="8" cy="8" r= "2" fill="#000000"/>
+    </svg>
+  )
+}
