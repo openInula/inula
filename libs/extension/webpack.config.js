59e00b336a7250684230c4bb563b05d60439af73
* <8>
2022-04-15 17:47:16 +0800

    Match-id-b327b25eaf26637343f8b71448ac68905ebe3ded

---
+++ libs/extension/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+const config = {
+  entry: {
+    background: './src/background/index.ts',
+    main: './src/main/index.ts',
+    injector: './src/injector/index.ts',
+    contentScript: './src/contentScript/index.ts',
+    panel: './src/panel/index.tsx',
+    popup: './src/popup/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].js',
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          }
+        ]
+      },
+      {
+        test: /\.less/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+
+            }
+          },
+          'less-loader'],
+      }]
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  externals: {
+    'horizon': 'Horizon',
+  },
+};
+
+module.exports = config;
