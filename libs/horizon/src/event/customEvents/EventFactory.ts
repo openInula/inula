d6bf291159af7beec6dc9192250a29b117eff593
* <8>
2022-05-24 19:19:05 +0800

    Match-id-6abacb1c502f0f8703b691e05fb5e97858f10d37

---
+++ libs/horizon/src/event/customEvents/EventFactory.ts
@@ -0,0 +1,36 @@
+
+// 兼容IE的event key
+const uniqueKeyMap = new Map([
+  ['Esc', 'Escape'],
+  ['Spacebar', ' '],
+  ['Left', 'ArrowLeft'],
+  ['Up', 'ArrowUp'],
+  ['Right', 'ArrowRight'],
+  ['Down', 'ArrowDown'],
+  ['Del', 'Delete'],
+]);
+
+const noop = () => {};
+// 创建普通自定义事件对象实例，和原生事件对应
+export function decorateNativeEvent(customEventName, nativeEvtName, nativeEvent) {
+
+  nativeEvent.isDefaultPrevented = () => nativeEvent.defaultPrevented;
+  nativeEvent.isPropagationStopped = () => nativeEvent.cancelBubble;
+  // 适配老版本事件api
+  nativeEvent.persist = noop;
+
+  // custom事件自定义属性
+  nativeEvent.customEventName = customEventName;
+  nativeEvent.nativeEvent = nativeEvent;
+  // 保存原生的事件类型，因为下面会修改
+  nativeEvent.nativeEventType = nativeEvent.type;
+
+  Object.defineProperty(nativeEvent, 'type', { writable: true });
+  nativeEvent.type = nativeEvtName;
+
+  const orgKey = nativeEvent.key;
+  Object.defineProperty(nativeEvent, 'key', { writable: true });
+  nativeEvent.key = uniqueKeyMap.get(orgKey) || orgKey;
+
+  return nativeEvent;
+}
---
+++ libs/horizon/src/event/simulatedEvtHandler/ChangeEventHandler.ts
@@ -0,0 +1,60 @@
+import {decorateNativeEvent} from '../customEvents/EventFactory';
+import {getDom} from '../../dom/DOMInternalKeys';
+import {isInputValueChanged} from '../../dom/valueHandler/ValueChangeHandler';
+import {addValueUpdateList} from '../ControlledValueUpdater';
+import {isInputElement} from '../utils';
+import {EVENT_TYPE_ALL} from '../const';
+import {AnyNativeEvent, ListenerUnitList} from '../Types';
+import {
+  getListenersFromTree,
+} from '../ListenerGetter';
+import {VNode} from '../../renderer/Types';
+import {getDomTag} from '../../dom/utils/Common';
+
+// 返回是否需要触发change事件标记
+function shouldTriggerChangeEvent(targetDom, evtName) {
+  const { type } = targetDom;
+  const domTag = getDomTag(targetDom);
+
+  if (domTag === 'select' || (domTag === 'input' && type === 'file')) {
+    return evtName === 'change';
+  } else if (domTag === 'input' && (type === 'checkbox' || type === 'radio')) {
+    if (evtName === 'click') {
+      return isInputValueChanged(targetDom);
+    }
+  } else if (isInputElement(targetDom)) {
+    if (evtName === 'input' || evtName === 'change') {
+      return isInputValueChanged(targetDom);
+    }
+  }
+  return false;
+}
+
+/**
+ *
+ * 支持input/textarea/select的onChange事件
+ */
+export function getListeners(
+  nativeEvtName: string,
+  nativeEvt: AnyNativeEvent,
+  vNode: null | VNode,
+  target: null | EventTarget,
+): ListenerUnitList {
+  if (!vNode) {
+    return [];
+  }
+  const targetDom = getDom(vNode);
+
+  // 判断是否需要触发change事件
+  if (shouldTriggerChangeEvent(targetDom, nativeEvtName)) {
+    addValueUpdateList(target);
+    const event = decorateNativeEvent(
+      'onChange',
+      'change',
+      nativeEvt,
+    );
+    return getListenersFromTree(vNode, 'onChange', event, EVENT_TYPE_ALL);
+  }
+
+  return [];
+}
