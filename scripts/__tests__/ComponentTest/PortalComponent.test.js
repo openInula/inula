8e006f8cd26b46e59906694406f75657d2f2c84d
* <8>
2022-09-13 16:36:55 +0800

    Match-id-3d22079ded1ca2a343f8859618146c4dc7abe9a7

--- scripts/__tests__/ComponentTest/PortalComponent.test.js
+++ scripts/__tests__/ComponentTest/PortalComponent.test.js
@@ -15,12 +15,10 @@ describe('PortalComponent Test', () => {
       }
 
       render() {
-        return Horizon.createPortal(
-          this.props.child,
-          this.element,
-        );
+        return Horizon.createPortal(this.props.child, this.element);
       }
     }
+
     Horizon.render(<PortalApp child={<div>PortalApp</div>} />, container);
     expect(container.textContent).toBe('');
     // <div>PortalApp</div>被渲染到了portalRoot而非container
@@ -44,17 +42,12 @@ describe('PortalComponent Test', () => {
 
       render() {
         return [
-          Horizon.createPortal(
-            this.props.child,
-            this.element,
-          ),
-          Horizon.createPortal(
-            this.props.child,
-            this.newElement,
-          )
+          Horizon.createPortal(this.props.child, this.element),
+          Horizon.createPortal(this.props.child, this.newElement),
         ];
       }
     }
+
     Horizon.render(<PortalApp child={<div>PortalApp</div>} />, container);
     expect(container.textContent).toBe('');
     // <div>PortalApp</div>被渲染到了portalRoot而非container
@@ -83,21 +76,16 @@ describe('PortalComponent Test', () => {
       render() {
         return [
           <div>PortalApp1st</div>,
-          Horizon.createPortal([
-            <div>PortalApp4</div>,
-            Horizon.createPortal(
-              this.props.child,
-              this.element3rd,
-            ),
-          ], this.element),
-          <div>PortalApp2nd</div>,
           Horizon.createPortal(
-            this.props.child,
-            this.newElement,
-          )
+            [<div>PortalApp4</div>, Horizon.createPortal(this.props.child, this.element3rd)],
+            this.element
+          ),
+          <div>PortalApp2nd</div>,
+          Horizon.createPortal(this.props.child, this.newElement),
         ];
       }
     }
+
     Horizon.render(<PortalApp child={<div>PortalApp</div>} />, container);
     expect(container.textContent).toBe('PortalApp1stPortalApp2nd');
     // <div>PortalApp4</div>会挂载在this.element上
@@ -121,25 +109,23 @@ describe('PortalComponent Test', () => {
       }
 
       render() {
-        return Horizon.createPortal(
-          this.props.child,
-          this.element,
-        );
+        return Horizon.createPortal(this.props.child, this.element);
       }
     }
-    Horizon.render(<PortalApp key='portal' child={<div>PortalApp</div>} />, container);
+
+    Horizon.render(<PortalApp key="portal" child={<div>PortalApp</div>} />, container);
     expect(container.textContent).toBe('');
     expect(portalRoot.textContent).toBe('PortalApp');
 
-    Horizon.render(<PortalApp key='portal' child={<div>AppPortal</div>} />, container);
+    Horizon.render(<PortalApp key="portal" child={<div>AppPortal</div>} />, container);
     expect(container.textContent).toBe('');
     expect(portalRoot.textContent).toBe('AppPortal');
 
-    Horizon.render(<PortalApp key='portal' child={['por', 'tal']} />, container);
+    Horizon.render(<PortalApp key="portal" child={['por', 'tal']} />, container);
     expect(container.textContent).toBe('');
     expect(portalRoot.textContent).toBe('portal');
 
-    Horizon.render(<PortalApp key='portal' child={null} />, container);
+    Horizon.render(<PortalApp key="portal" child={null} />, container);
     expect(container.textContent).toBe('');
     expect(portalRoot.textContent).toBe('');
 
@@ -159,10 +145,7 @@ describe('PortalComponent Test', () => {
       }
 
       render() {
-        return Horizon.createPortal(
-          this.props.child,
-          this.element,
-        );
+        return Horizon.createPortal(this.props.child, this.element);
       }
     }
 
@@ -173,7 +156,6 @@ describe('PortalComponent Test', () => {
         </div>
       );
     };
-
 
     const App = () => {
       const handleClick = () => {
@@ -186,9 +168,7 @@ describe('PortalComponent Test', () => {
 
       return (
         <div onClickCapture={handleCaptureClick()} onClick={handleClick()}>
-          <PortalApp child={<Child />}>
-
-          </PortalApp>
+          <PortalApp child={<Child />}></PortalApp>
         </div>
       );
     };
@@ -200,24 +180,23 @@ describe('PortalComponent Test', () => {
     expect(LogUtils.getAndClear()).toEqual([
       // 从外到内先捕获再冒泡
       'capture click event',
-      'bubble click event'
+      'bubble click event',
     ]);
   });
 
   it('Create portal at app root should not add event listener multiple times', () => {
-  const btnRef = Horizon.createRef();
+    const btnRef = Horizon.createRef();
+
     class PortalApp extends Horizon.Component {
       constructor(props) {
         super(props);
       }
 
       render() {
-        return Horizon.createPortal(
-          this.props.child,
-          container,
-        );
+        return Horizon.createPortal(this.props.child, container);
       }
     }
+
     const onClick = jest.fn();
 
     class App extends Horizon.Component {
@@ -226,18 +205,21 @@ describe('PortalComponent Test', () => {
       }
 
       render() {
-        return <div>
-          <button onClick={onClick} ref={btnRef}></button>
-          <PortalApp />
-        </div>;
+        return (
+          <div>
+            <button onClick={onClick} ref={btnRef}></button>
+            <PortalApp />
+          </div>
+        );
       }
     }
+
     Horizon.render(<App />, container);
     btnRef.current.click();
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
-  it('Portal onChange should activate', () => {
+  it('#76 Portal onChange should activate', () => {
     class Dialog extends Horizon.Component {
       node;
 
@@ -246,7 +228,8 @@ describe('PortalComponent Test', () => {
         this.node = window.document.createElement('div');
         window.document.body.appendChild(this.node);
       }
-``      render() {
+
+      render() {
         return Horizon.createPortal(this.props.children, this.node);
       }
     }
@@ -254,24 +237,35 @@ describe('PortalComponent Test', () => {
     let showPortalInput;
     const fn = jest.fn();
     const inputRef = Horizon.createRef();
-    function Input() {
-      const [show, setShow] = Horizon.useState(false);
-      showPortalInput = setShow;
 
-      Horizon.useEffect(() => {
-        setTimeout(() => {
-          setShow(true);
-        }, 0);
-      }, []);
+    function App() {
+      const Input = () => {
+        const [show, setShow] = Horizon.useState(false);
+        showPortalInput = setShow;
 
-      if (!show) {
-        return null;
-      }
+        Horizon.useEffect(() => {
+          setTimeout(() => {
+            setShow(true);
+          }, 0);
+        }, []);
 
-      return <input onChange={fn} ref={inputRef}/>;
+        if (!show) {
+          return null;
+        }
+
+        return <input onChange={fn} ref={inputRef} />;
+      };
+
+      return (
+        <div>
+          <Dialog>
+            <Input />
+          </Dialog>
+        </div>
+      );
     }
 
-    Horizon.render(<div><Dialog><App /></Dialog>, container);
+    Horizon.render(<App />, container);
     showPortalInput(true);
     jest.advanceTimersToNextTimer();
     dispatchChangeEvent(inputRef.current, 'test');
