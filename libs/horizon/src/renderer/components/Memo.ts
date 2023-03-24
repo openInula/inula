639a797ef1c57945de30b6f1dc9cb6ea788587df
* <*>
2023-03-24 17:22:30 +0800

    Match-id-a0e9ecd2323caf055755c2e5a907dffd3a6881a5

--- libs/horizon/src/renderer/components/Memo.ts
+++ libs/horizon/src/renderer/components/Memo.ts
@@ -17,6 +17,7 @@ import { TYPE_MEMO } from '../../external/JSXElementType';
 
 export function memo<Props>(type, compare?: (oldProps: Props, newProps: Props) => boolean) {
   const memoJSXElement = {
+    vtype: TYPE_MEMO,
     $$typeof: TYPE_MEMO, // 规避三方件hoist-non-react-statics中，通过$$typeof获取类型，但获取不到，导致type被覆盖
     type: type,
     compare: compare === undefined ? null : compare,
@@ -26,7 +27,6 @@ export function memo<Props>(type, compare?: (oldProps: Props, newProps: Props) =
   Object.defineProperty(memoJSXElement, 'vtype', {
     configurable: false,
     writable: false,
-    value: TYPE_MEMO,
   });
 
   return memoJSXElement;
