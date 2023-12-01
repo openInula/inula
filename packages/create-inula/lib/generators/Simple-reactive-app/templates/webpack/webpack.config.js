45ebc9b97d72ecca14407f26a5eef696d5413bde
13659257719 <819781841@qq.com>
2023-12-01 14:40:05 +0800

    [create-inula]<feat> 新增响应式 API 模板

--- packages/create-inula/lib/generators/Simple-reactive-app/templates/webpack/webpack.config.js
+++ packages/create-inula/lib/generators/Simple-reactive-app/templates/webpack/webpack.config.js
@@ -17,7 +17,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
