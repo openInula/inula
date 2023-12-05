706c98c7a0a432b70a81dd2fcc149d3cec15660a
13659257719 <819781841@qq.com>
2023-12-05 19:38:17 +0800

    [inula-request]<bugfix> 请求组件类型导出优化

--- packages/inula-request/index.ts
+++ packages/inula-request/index.ts
@@ -68,7 +68,7 @@ export {
   isAxiosError,
 };
 
-export type { IrRequestConfig, IrResponse, IrInstance, CancelTokenSource, IrProgressEvent } from './src/types/interfaces';
-export type { Method, ResponseType } from './src/types/types';
+export { IrRequestConfig, IrResponse, IrInstance, CancelTokenSource, IrProgressEvent } from './src/types/interfaces';
+export { Method, ResponseType } from './src/types/types';
 
 export default inulaRequest;
