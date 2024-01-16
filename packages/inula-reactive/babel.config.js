5dffe769d6d091bb80566bbb68832a5becf4b2e5
* <*>
2024-01-16 19:23:14 +0800

    Match-id-d713896a7b138a13e9d49999c3a11346c52f5273

--- packages/inula-reactive/babel.config.js
+++ packages/inula-reactive/babel.config.js
@@ -14,36 +14,12 @@
  */
 
 module.exports = {
-  presets: ['@babel/preset-typescript', ['@babel/preset-env', { targets: { node: 'current' } }]],
-  plugins: [
-    '@babel/plugin-syntax-jsx',
+  presets: [
     [
-      '@babel/plugin-transform-react-jsx',
-      {
-        pragma: 'Inula.createElement',
-        pragmaFrag: 'Inula.Fragment',
-      },
+      '@babel/preset-env',
     ],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-    '@babel/plugin-transform-object-assign',
-    '@babel/plugin-transform-object-super',
-    ['@babel/plugin-proposal-object-rest-spread', { loose: true, useBuiltIns: true }],
-    ['@babel/plugin-transform-template-literals', { loose: true }],
-    '@babel/plugin-transform-arrow-functions',
-    '@babel/plugin-transform-literals',
-    '@babel/plugin-transform-for-of',
-    '@babel/plugin-transform-block-scoped-functions',
-    '@babel/plugin-transform-classes',
-    '@babel/plugin-transform-shorthand-properties',
-    '@babel/plugin-transform-computed-properties',
-    '@babel/plugin-transform-parameters',
-    ['@babel/plugin-transform-spread', { loose: true, useBuiltIns: true }],
-    ['@babel/plugin-transform-block-scoping', { throwIfClosureRequired: false }],
-    ['@babel/plugin-transform-destructuring', { loose: true, useBuiltIns: true }],
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-optional-chaining',
-  ],
+    [
+      '@babel/preset-typescript',
+    ]
+  ]
 };
---
+++ packages/inula-reactive/src/RNodeCreator.ts
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2020 Huawei Technologies Co.,Ltd.
+ *
+ * openGauss is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+import { isPrimitive } from './Utils';
+import { RNode } from './RNode';
+
+export type Reactive<T = any> = RNode<T> | Atom<T>;
+
+export function createReactive<T extends any>(raw?: T): ReactiveProxy<T> {
+  if (isPrimitive(raw) || raw === null || raw === undefined) {
+    return new RNode(raw, { isSignal: true });
+  } else {
+    const node = new RNode(null, {
+      isProxy: true,
+      root: { $: raw },
+    });
+    return node.proxy as ReactiveProxy<T>;
+  }
+}
+
+export function createComputed<T>(fn: T) {
+  const rNode = new RNode(fn, { isProxy: true, isComputed: true });
+  return rNode.proxy;
+}
+
+export function createWatch<T>(fn: T) {
+  const rNode = new RNode(fn, {
+    isEffect: true,
+  });
+
+  rNode.get();
+}
+
+export function getOrCreateChildProxy(
+  value: unknown,
+  parent: RNode<any>,
+  key: string | symbol
+): Atom | ProxyRNode<any> {
+  const child = getOrCreateChildRNode(parent, key);
+
+  return child.proxy;
+}
+
+export function getOrCreateChildRNode(node: RNode<any>, key: string | symbol): RNode<any> {
+  let child = node.children?.get(key);
+
+  if (!child) {
+    child = new RNode(null, {
+      isProxy: true,
+      parent: node,
+      key: key,
+      root: node.root,
+    });
+  }
+
+  return child;
+}
