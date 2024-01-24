0903248e2f969ba1580dbe117a132e25cc90d895
13659257719 <819781841@qq.com>
2024-01-24 15:23:16 +0800

    fix(request): 优化请求参数使用体验和解决 URL 拼接问题

--- packages/inula-request/examples/server/serverTest.mjs
+++ packages/inula-request/examples/server/serverTest.mjs
@@ -17,7 +17,7 @@ import express from "express";
 import * as fs from "fs";
 import bodyParser from "body-parser";
 import cors from "cors";
-import path from "path";
+import * as path from "path";
 
 const app = express();
 const port = 3001;
