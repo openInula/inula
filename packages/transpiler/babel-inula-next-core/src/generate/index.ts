51438d720e79af51943792f1e49504e9cc40ee27
Hoikan <30694822+HoikanChan@users.noreply.github.com>
2024-05-29 17:40:16 +0800

    chore: remove unused dir

--- packages/transpiler/babel-inula-next-core/src/generate/index.ts
+++
@@ -1,9 +0,0 @@
-import { ViewParticle } from '@openinula/reactivity-parser';
-import { ComponentNode } from '../analyze/types';
-
-type Visitor = {
-  [Type in (ViewParticle | ComponentNode)['type']]: (
-    node: Extract<ViewParticle | ComponentNode, { type: Type }>,
-    ctx: any
-  ) => void;
-};
