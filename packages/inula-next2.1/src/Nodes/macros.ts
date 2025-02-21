06f09da1fd5a6559c81ed208f7c092b508dbd5a8
Hoikan <408255371@qq.com>
2025-02-21 11:27:40 +0800

    feat(API-2.1): support untrack

--- packages/inula-next2.1/src/Nodes/macros.ts
+++ packages/inula-next2.1/src/Nodes/macros.ts
@@ -1,15 +1,24 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function watch(effect: () => void) {
   throw new Error('Watch should not be called directly, please check the docs for more information');
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function didMount(effect: () => void) {
   throw new Error('DidMount should not be called directly, please check the docs for more information');
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function willUnmount(effect: () => void) {
   throw new Error('WillUnmount should not be called directly, please check the docs for more information');
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function didUnmount(effect: () => void) {
   throw new Error('DidUnmount should not be called directly, please check the docs for more information');
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function untrack<T>(getter: () => T): T {
+  return getter();
 }
